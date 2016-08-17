0cabb591be59e9781ea81539617e232a94ce2ca9
丁叫兽 <ding_tk@163.com>
2016-08-17 18:51:54 +0800

    视频播放器
    视频播放器

---
+++ JZVideoDemo/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  JZVideoDemo
+//
+//  Created by jinzelu on 15/6/2.
+//  Copyright (c) 2015年 jinzelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property(nonatomic, assign) BOOL isFullScreen;
+
+@end
+
